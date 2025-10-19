a8e27769c35d89b26db1d8fe22265b43c648a088
Chamidi Lakshika <mclakshi@gmail.com>
2025-10-19 23:30:03 +0530

    updated(wep)

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-
 // Redirect root to students page
 Route::get('/', function () {
     return redirect()->route('students.index');
